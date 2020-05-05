563c61acf9ad646f2872dd73e30dadba8c84320c
Hsin-Yu <nuphoto.hsinyu@gmail.com>
2020-05-05 10:41:56 +0800

    Setup basic configuration (indent, output entry)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -14,5 +14,15 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 4],
+    "class-methods-use-this": "off",
+    "vue/html-indent": ["error", 4, {
+      "attribute": 1,
+      "baseIndent": 1,
+      "closeBracket": 0,
+      "alignAttributesVertically": true,
+      "ignores": []
+    }],
   },
 };
