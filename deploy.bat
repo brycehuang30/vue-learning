3a6e6f875667c91079e6a5e6ef685f16bb5bc302
Lohi <lohi3152@gmail.com>
2020-05-08 16:56:14 +0800

    deploy

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+xcopy C:\Users\305\Desktop\vue-learning\dist\* C:\Users\305\Desktop\"a-btn vue"\ /Y/E
+cd C:\Users\305\Desktop\"a-btn vue"
+git add -A
+git commit -m "push"
+git push heroku master
+pause
